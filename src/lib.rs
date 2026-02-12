36b1dc0a25be0a2764e1fc44e4606bd1f755071a
Chris Reister <creister@cavulus.com>
2026-02-12 09:20:48 -0800

    rebased to original branch

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,7 @@ pub use algorithms::{Algorithm, AlgorithmFamily};
 pub use decoding::{DecodingKey, DecodingKeyKind, TokenData, decode, decode_header};
 pub use encoding::{EncodingKey, encode};
 pub use header::{Header, HeaderValue};
+pub use signature;
 pub use validation::{Validation, get_current_timestamp};
 
 /// Dangerous decoding functions that should be audited and used with extreme care.
